9cc72af6073b73e109446f4fee6baf586a305f1b
beatz-steph <otenku123456@gmail.com>
2019-10-08 11:24:25 +0100

    Updates

---
+++ precache-manifest.cbec4001bc4c244d56fae39befb0f66c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37811f18d1c5bd348b5b9712df3e8ff0",
+    "url": "beatz-steph.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d3a912f6aebb768efe70",
+    "url": "beatz-steph.io/monsters-rolodex/static/css/main.e358a966.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "beatz-steph.io/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "d3a912f6aebb768efe70",
+    "url": "beatz-steph.io/monsters-rolodex/static/js/main.a3162d74.chunk.js"
+  },
+  {
+    "revision": "5e239cbef9fc141d9927",
+    "url": "beatz-steph.io/monsters-rolodex/static/js/runtime-main.569b69c2.js"
+  }
+]);
